0378f15f46a5f1c8dce83c03f9adad950592dc35
Dylan Warren <duwtgb@mst.edu>
2018-09-29 01:25:35 -0500

    Add a sleep to not make the bot isnta send

--- commands/tag.ts
+++ commands/tag.ts
@@ -1,12 +1,16 @@
 import { Message } from "discord.js";
 
+function sleep(ms: number = 0) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
 export default {
   alias: ['tag'],
   run: async (message: Message) => {
     let keys = Array.from(message.guild.members.keys())
     const member = message.guild.members.get(keys[Math.floor(Math.random() * keys.length)])
     message.channel.send(":thinking:...")
-    setTimeout(() => {}, 1000)
-    message.channel.send(`${member!.user} tag!`)
+    await sleep(1000)
+    message.channel.send(`${member!.user} tag; you're it!`)
   }
 }
