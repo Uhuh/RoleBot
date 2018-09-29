b2f9b59f4734eaa168f6c1c337b287d1659bdecc
Dylan Warren <duwtgb@mst.edu>
2018-09-29 00:52:40 -0500

    Stupid random tag game

--- commands/cmd.ts
+++ commands/cmd.ts
@@ -1,9 +1,10 @@
 import addRole from './addRole'
 import addChannel from './addChannel'
 import removeChannel from './removeChannel'
+import tag from './tag';
 
 let commandsMap = new Map()
-let list  = [addRole, addChannel, removeChannel]
+let list  = [addRole, addChannel, removeChannel, tag]
 
 for (const i of list) {
   for (const j of i.alias) {
---
+++ commands/tag.ts
@@ -0,0 +1,10 @@
+import { Message } from "discord.js";
+
+export default {
+  alias: ['tag'],
+  run: async (message: Message) => {
+    let keys = Array.from(message.guild.members.keys())
+    const member = message.guild.members.get(keys[Math.floor(Math.random() * keys.length)])
+    message.channel.send(`@${member!.user.username} tag!`)
+  }
+}
