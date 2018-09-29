eb3e4ba3361e71e57f683b95fcd00706467ba0b7
Dylan Warren <duwtgb@mst.edu>
2018-09-29 18:34:31 -0500

    I like status info

--- commands/cmd.ts
+++ commands/cmd.ts
@@ -2,9 +2,10 @@ import addRole from './addRole'
 import addChannel from './addChannel'
 import removeChannel from './removeChannel'
 import tag from './tag';
+import status from './status';
 
 let commandsMap = new Map()
-let list  = [addRole, addChannel, removeChannel, tag]
+let list  = [addRole, addChannel, removeChannel, tag, status]
 
 for (const i of list) {
   for (const j of i.alias) {
---
+++ commands/status.ts
@@ -0,0 +1,36 @@
+
+import { Message, RichEmbed, Role, Guild } from "discord.js"
+
+export default {
+  desc: 'Gets the server info',
+  common: 'status',
+  args: '',
+  alias: ['status', 'serverinfo', 'guild', 'server'],
+  run: async function (message: Message) {
+    const embed: RichEmbed = new RichEmbed()
+    const guild: Guild = message.guild
+    let roles: Role[] = []
+    let textC: number = 0
+    let voiceC: number = 0
+    if (message.channel.type === 'dm') return
+
+    for (const [k, role] of guild.roles) { roles.push(role) }
+
+    for (const [k, channel] of guild.channels) {
+      if (channel.type === 'text') textC++
+      else if (channel.type === 'voice') voiceC++
+    }
+
+    embed.setColor(16772864)
+      .setThumbnail(guild.iconURL)
+      .setDescription(`**Server information for _${guild.name}_**`)
+      .addField(`_**> Owner**_`, `\`${guild.owner.user.tag}\``, true)
+      .addField(`_**> OwnerID**_`, `\`${guild.ownerID}\``, true)
+      .addField(`_**> Users**_`, `\`${guild.memberCount}\``, true)
+      .addField(`_**> Text Channels**_`, `\`${textC}\``, true)
+      .addField(`_**> Voice Channels**_`, `\`${voiceC}\``, true)
+      .addField(`_**> Roles** from highest to lowest_`, `${roles.reverse().join(' ')}`, true)
+
+    message.channel.send(embed)
+  }
+}
