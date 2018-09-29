43ef7dbe21d5c450f2c6227a13bade569fdb03d0
Dylan Warren <duwtgb@mst.edu>
2018-09-29 00:59:16 -0500

    Have to use user id not handle name

--- commands/tag.ts
+++ commands/tag.ts
@@ -5,6 +5,6 @@ export default {
   run: async (message: Message) => {
     let keys = Array.from(message.guild.members.keys())
     const member = message.guild.members.get(keys[Math.floor(Math.random() * keys.length)])
-    message.channel.send(`@${member!.user.username} tag!`)
+    message.channel.send(`@${member!.user.id} tag!`)
   }
 }
