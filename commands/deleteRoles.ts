8e2aa83adf42dbaa4fa714512a421d148f19148e
Dylan Warren <duwtgb@mst.edu>
2018-10-18 17:34:38 -0500

    no

--- commands/deleteRoles.ts
+++ commands/deleteRoles.ts
@@ -5,11 +5,11 @@ import { Message, Guild } from "discord.js"
   alias: ['deleteRoles'], 
   run: (message: Message, args: string[], client: Bowsette) => {
     // ignore them plebians
-    if (!message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return;
+    if (!message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return message.reply("no stop");
 
     message.reply('sure hope you wanted to do this.')
     client.deleteRoles.run(message.guild.id)
     message.react("✅")
-    
+
   }
 }
