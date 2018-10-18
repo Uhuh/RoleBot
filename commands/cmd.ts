5350c6cbd340b697969fb2211150b244b74fead5
Dylan Warren <duwtgb@mst.edu>
2018-10-18 17:35:59 -0500

    Wow I'm dumb

--- commands/cmd.ts
+++ commands/cmd.ts
@@ -3,9 +3,10 @@ import addChannel from './addChannel'
 import removeChannel from './removeChannel'
 import tag from './tag';
 import status from './status';
+import deleteRoles from './deleteRoles';
 
 let commandsMap = new Map()
-let list  = [addRole, addChannel, removeChannel, tag, status]
+let list  = [addRole, addChannel, removeChannel, tag, status, deleteRoles]
 
 for (const i of list) {
   for (const j of i.alias) {
--- commands/deleteRoles.ts
+++ commands/deleteRoles.ts
@@ -5,7 +5,7 @@ import { Message, Guild } from "discord.js"
   alias: ['deleteRoles'], 
   run: (message: Message, args: string[], client: Bowsette) => {
     // ignore them plebians
-    //if (!message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return message.reply("no stop");
+    if (!message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return message.reply("no stop");
 
     message.reply('sure hope you wanted to do this.')
     client.deleteRoles.run(message.guild.id)
