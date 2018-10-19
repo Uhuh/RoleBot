205a2b31667347d45b8a5bf8871b9a8ad583972e
Dylan Warren <duwtgb@mst.edu>
2018-10-18 21:51:35 -0500

    Now grants roles with spaces. oops

--- commands/roles.ts
+++ commands/roles.ts
@@ -1,31 +1,32 @@
 import Bowsette from "../src/bot"
 import { Message, GuildMember } from "discord.js"
 export default async function (client: Bowsette, message: Message) {
-  const member: GuildMember = message.member
-  const words: string[] = message.content.split(' ')
+  const member = message.member
+  const role = message.content
   const guildRoles = client.getRoles.all(message.guild.id)
   let addedRole: boolean = false
   let req_role: string = ""
   message.delete()
-  for (const i of words) {
-    if(member.roles.find(val => val.name === i)) {
-      return
-    }
+
+  // IF they already have the role, ignore.
+  if(member.roles.find(val => val.name === role)) {
+    return
   }
-  for (const word of words) {
-    for (const gR of guildRoles) {
-      if (gR.role_name && gR.role_name.toLowerCase() === word.toLowerCase()) {
-        req_role = word.toLowerCase()
-        addedRole = await (member.addRole(gR.role_id)
-        .then(() => { return true })
-        .catch(() => { return false }))
-        // if a secondary role don't remove any other
-        if (gR.prim_role === 0) {
-          return
-        }
-        break
+
+  // Make sure the server has the role to give.
+  for (const gR of guildRoles) {
+    if (gR.role_name && gR.role_name.toLowerCase() === role.toLowerCase()) {
+      req_role = role.toLowerCase()
+      addedRole = await (member.addRole(gR.role_id)
+      .then(() => { return true })
+      .catch(() => { return false }))
+      // if a secondary role don't remove previously given role.
+      if (gR.prim_role === 0) {
+        return
       }
+      break
     }
+    // No need to loop again if role was granted.
     if (addedRole) break
   }
   for (const [k, role] of member.roles) {
