4bff8731a847b58cfbf269aa8301d2638aeb73a1
Dylan Warren <duwtgb@mst.edu>
2019-04-11 22:35:27 -0500

    Join roles with a space, SMH

--- commands/callable/deleteRoles.ts
+++ commands/callable/deleteRoles.ts
@@ -2,12 +2,12 @@ import { Message } from "discord.js";
 import { deleteRole } from "../../src/setup_table";
 
 export default {
-  desc: "Delete a single role from your hand out roles list. ",
+  desc: "Delete a single role from your hand out roles list.",
   name: "deleteRole",
   args: "<role name>",
   run: (message: Message, args: string[]) => {
     if (!message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return;
-    const name = args.join("");
+    const name = args.join(" ");
     const guildID = message.guild.id;
     
     deleteRole.run(guildID, name);
