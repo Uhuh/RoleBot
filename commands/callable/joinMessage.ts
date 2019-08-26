0f47f78c15300b650c00802c7f3bd1fb09ac7dd1
Quinzell <jared.rainwater@monsanto.com>
2019-08-25 20:47:22 -0500

    react

--- commands/callable/joinMessage.ts
+++ commands/callable/joinMessage.ts
@@ -6,7 +6,9 @@ export default {
     args: '<message> [roleName reaction]...',
     run: async (originalMessage: Message, _args: string[]) => {
         const [message, ...rest] = _args;
-        const roleReaction = rest.join(' ').split(/\w+\W\w+/)
-        originalMessage.channel.sendMessage(message + roleReaction.join('=>'))
+        const roleReactions = rest.join(' ').split(/\w+\W\w+/)
+        const reactions = roleReactions.map((roleReaction) => roleReaction.split(' ')).map(([,reaction]) => reaction)
+        const sentMessage = await originalMessage.channel.sendMessage(message + '\n' + roleReactions.join('=>')) as Message
+        reactions.forEach(reaction => sentMessage.react(reaction))
     }
 }
