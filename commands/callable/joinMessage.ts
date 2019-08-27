2615067493a1d47cb183694e6af109b8f82cf216
Quinzell <>
2019-08-27 08:32:37 -0500

    working on joinMessage

--- commands/callable/joinMessage.ts
+++ commands/callable/joinMessage.ts
@@ -1,4 +1,5 @@
 import { Message, Emoji } from "discord.js";
+import {addReactionRole} from '../../src/setup_table'
 
 export default {
     desc: 'Create a message with reactions that toggle roole assignment',
@@ -7,7 +8,7 @@ export default {
     run: async (originalMessage: Message, _args: string[]) => {
 		const [, ,...words] = originalMessage.content.split(/\s+/)
 		const roles = words.filter((word, i) => word && i % 2 === 0)
-		const reactions = words.filter((word, i) => word && i % 2 !== 0).map(reaction => {
+		const reactions: any[] = words.filter((word, i) => word && i % 2 !== 0).map(reaction => {
 			const match = /<:\w+:(\d+)>/.exec(reaction)
 			if(match) {
 				const [,id] = match
@@ -15,9 +16,21 @@ export default {
 			}
 			return reaction
 		})
+		const roleReactionMap: any = roles.reduce((map, role, i) => ({
+			...map,
+			[role]: reactions[i].id || reactions[i]
+		}), {})
         const roleReactions = roles.map((role, i) => role + " => " + reactions[i]) 
         const sentMessage = await originalMessage.channel.sendMessage(roleReactions.join('\n')) as Message
-		// get MessageReaction from .react and send .emoji.id to db?
-        reactions.forEach(reaction => sentMessage.react(reaction))
+		// get MessageReaction from .react and send .emoji.id or .emoji.name to db?
+		// get role id. Same role from db???
+        const messageReactions = await Promise.all(reactions.map(reaction => sentMessage.react(reaction)))
+		messageReactions.forEach(r => {
+			console.log(r && r.emoji)
+			const id = r.emoji.id || r.emoji.name
+			const roleName = roleReactionMap[id]
+			const role = originalMessage.guild.roles.find(({name}) => name === roleName)
+			//addReactionRole.run({emoji_id: id, role_id: role.id})
+		})
     }
 }
