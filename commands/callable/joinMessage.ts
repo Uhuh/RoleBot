4b394a1e3f8c89dc02a34dd63ed114f508951364
Quinzell <>
2019-08-26 19:43:08 -0500

    parse emoji reactions

--- commands/callable/joinMessage.ts
+++ commands/callable/joinMessage.ts
@@ -1,14 +1,22 @@
-import { Message } from "discord.js";
+import { Message, Emoji } from "discord.js";
 
 export default {
     desc: 'Create a message with reactions that toggle roole assignment',
     name: 'joinMessage',
-    args: '<message> [roleName reaction]...',
+    args: '[roleName reaction]...',
     run: async (originalMessage: Message, _args: string[]) => {
-        const [message, ...rest] = _args;
-        const roleReactions = rest.join(' ').split(/\w+\W\w+/)
-        const reactions = roleReactions.map((roleReaction) => roleReaction.split(' ')).map(([,reaction]) => reaction)
-        const sentMessage = await originalMessage.channel.sendMessage(message + '\n' + roleReactions.join('=>')) as Message
+		const [, ,...words] = originalMessage.content.split(/\s+/)
+		const roles = words.filter((word, i) => word && i % 2 === 0)
+		const reactions = words.filter((word, i) => word && i % 2 !== 0).map(reaction => {
+			const match = /<:\w+:(\d+)>/.exec(reaction)
+			if(match) {
+				const [,id] = match
+				return originalMessage.guild.emojis.get(id) as Emoji
+			}
+			return reaction
+		})
+        const roleReactions = roles.map((role, i) => role + " => " + reactions[i]) 
+        const sentMessage = await originalMessage.channel.sendMessage(roleReactions.join('\n')) as Message
         reactions.forEach(reaction => sentMessage.react(reaction))
     }
 }
