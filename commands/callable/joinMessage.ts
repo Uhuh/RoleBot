842343110c30ef18eb1ab7cbebbe687ad8c66d7e
Quinzell <>
2019-08-27 22:16:28 -0500

    finally bitches be mine

--- commands/callable/joinMessage.ts
+++ commands/callable/joinMessage.ts
@@ -1,5 +1,5 @@
 import { Message, Emoji } from "discord.js";
-import {addReactionRole} from '../../src/setup_table'
+import {addReactionRole, addJoinMessage} from '../../src/setup_table'
 
 export default {
     desc: 'Create a message with reactions that toggle roole assignment',
@@ -16,21 +16,21 @@ export default {
 			}
 			return reaction
 		})
-		const roleReactionMap: any = roles.reduce((map, role, i) => ({
+		const reactionRoleMap: any = reactions.reduce((map, reaction, i) => ({
 			...map,
-			[role]: reactions[i].id || reactions[i]
+			[reaction.id || reaction]: roles[i]
 		}), {})
         const roleReactions = roles.map((role, i) => role + " => " + reactions[i]) 
         const sentMessage = await originalMessage.channel.sendMessage(roleReactions.join('\n')) as Message
+		addJoinMessage(sentMessage.id, sentMessage.channel.id)
 		// get MessageReaction from .react and send .emoji.id or .emoji.name to db?
 		// get role id. Same role from db???
         const messageReactions = await Promise.all(reactions.map(reaction => sentMessage.react(reaction)))
 		messageReactions.forEach(r => {
-			console.log(r && r.emoji)
 			const id = r.emoji.id || r.emoji.name
-			const roleName = roleReactionMap[id]
+			const roleName = reactionRoleMap[id]
 			const role = originalMessage.guild.roles.find(({name}) => name === roleName)
-			//addReactionRole.run({emoji_id: id, role_id: role.id})
+			addReactionRole(id, role.id)
 		})
     }
 }
