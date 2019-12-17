117297dbb49b07e1eeeecfe0b1c95b408c09d1ea
Dylan Warren <dwarren@intellifarms.com>
2019-12-16 23:56:26 -0600

    React role stuff

---
+++ commands/callable/eval.ts
@@ -0,0 +1,31 @@
+import { Message } from "discord.js"
+import RoleBot from "../../src/bot"
+
+export default {
+  desc: '',
+  name: 'eval',
+  args: '',
+  run: async (message: Message, args: string[], client: RoleBot) => {
+    if (message.author.id !== client.config.BOT_OWNER) return
+
+    const clean = (text: string) => {
+      if (typeof (text) === "string")
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
+      else
+        return text
+    }
+
+    try {
+      const code = args.join(" ")
+      let evaled = eval(code)
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled)
+
+      message.channel.send(clean(evaled), { code: "xl" })
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``)
+    }
+
+  }
+}
