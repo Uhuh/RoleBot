92a027467a441973cd5c11322be06a490c5a0aad
Dylan Warren <dwarren@intellifarms.com>
2019-12-30 22:24:02 -0600

    Allow dm's for the bot to get a better help command working

--- events/message.ts
+++ events/message.ts
@@ -1,31 +1,37 @@
-import { Message, Channel } from "discord.js"
-import RoleBot from "../src/bot"
-import roles from "../commands/events/roles"
-import commands from "../commands/callable/commands"
+import { Message, Channel } from "discord.js";
+import RoleBot from "../src/bot";
+import roles from "../commands/events/roles";
+import commands from "../commands/callable/commands";
 
 export default (client: RoleBot, message: Message) => {
-  // Ignore bots, also don't allow dm's. No reason for users to DM the bot
-  if(message.author.bot || !message.guild)
-    return
-  
-  const channel: Channel | undefined = message.channel
-  const role_channel = client.roleChannels.get(message.guild.id) || ''
+  const { guild } = message;
+  const { id } = guild || "";
+
+  // Ignore bots
+  if (message.author.bot) return;
+
+  const channel: Channel | undefined = message.channel;
+  const role_channel = client.roleChannels.get(id) || "";
 
   // Someone is trying to request a role (hopefully)
   if (channel.id === role_channel) {
-    roles(message)
-  } else if (message.guild && message.mentions.members.has(client.user.id)) {
-    const length: number = message.content.split(" ")[0].length
+    roles(message);
+  } else if (message.mentions.users.first().id === client.user.id) {
+    const length: number = message.content.split(" ")[0].length;
     // + 1 for the damn space.
-    const [command, ...args] = message.content.substring(length + 1).split(" ")
+    const [command, ...args] = message.content.substring(length + 1).split(" ");
+
     // Allow users to mention the bot only, this will return the list of commands in a private message
-    if (message.mentions.members.has(client.user.id) && !command) {
-      commands.run(message, args, client)
-    }
+    if (!command) return commands.run(message, args, client);
+
     //If the command isn't in the big ol' list.
-    if (!client.commands.has(command.toLowerCase()))
-      return console.log("Command DNE")
+    if (
+      !client.commands.has(command.toLowerCase()) ||
+      (!guild && command.toLowerCase() !== "help")
+    )
+      return console.log("Command DNE");
+      
     // Find the command and run it.
-    client.commands.get(command.toLowerCase())!.run(message, args, client)
+    client.commands.get(command.toLowerCase())!.run(message, args, client);
   }
-}
+};
