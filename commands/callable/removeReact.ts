d087fc90fe02474aa6032e6e8331b6bcff2d70f0
Dylan Warren <duwtgb@mst.edu>
2020-01-15 20:41:51 -0600

    User wanted to be able to delete all roles

--- commands/callable/removeReact.ts
+++ commands/callable/removeReact.ts
@@ -2,9 +2,9 @@ import { Message } from "discord.js";
 import { removeReactionRole, guildReactions } from "../../src/setup_table";
 
 export default {
-  desc: "Delete a reaction role.",
+  desc: "Delete a reaction role. Use `-all` to delete all current reactions.",
   name: "deleteReact",
-  args: "<role name>",
+  args: "<role name>\n\t-all",
   type: "reaction",
   run: (message: Message, args: string[]) => {
     setTimeout(() => {
@@ -12,15 +12,20 @@ export default {
     }, 5000);
     if (!message.guild || !message.member!.hasPermission(["MANAGE_ROLES"]))
       return message.react("❌");
-    const name = args.join(" ");
+    const arg = args.join(" ");
     const GUILD_ID = message.guild.id;
     const DB_ROLES = guildReactions(GUILD_ID).map(r => r.role_id);
     const ROLE = message.guild.roles.find(
-      r => r.name.toLowerCase() === name.toLowerCase()
+      r => r.name.toLowerCase() === arg.toLowerCase()
     );
 
     if (ROLE && DB_ROLES.includes(ROLE.id)) {
       removeReactionRole(ROLE.id);
+      return message.react("✅");
+    } else if (arg.includes("-all")) {
+      DB_ROLES.forEach(r => {
+        removeReactionRole(r)
+      });
       return message.react("✅");
     }
 
