c69b2751437d702e7b155175746c078ccee471fa
Dylan Warren <duwtgb@mst.edu>
2020-01-26 21:05:11 -0600

    Move reaction based commands into it's own folder and added a command handler

---
+++ commands/reactCommands/add.ts
@@ -0,0 +1,166 @@
+import { Message, TextChannel } from "discord.js";
+import { addReactionRole, guildReactions } from "../../src/setup_table";
+import RoleBot from "../../src/bot";
+
+export default {
+  desc: "Associate an emoji with a role.",
+  name: "-add",
+  args: "{ Follow the prompts given }",
+  type: "reaction",
+  run: async (message: Message, _args: string[], client: RoleBot) => {
+    if (!message.guild || !message.member!.hasPermission(["MANAGE_ROLES"]))
+      return;
+
+    const channel = message.channel;
+    let done = false;
+    const finished = () => { done = true; }
+
+    while (!done) {
+      await reactSetup(channel as TextChannel, message, client, finished).then(console.log).catch(console.error);
+    }
+  }
+};
+
+const reactSetup = (channel: TextChannel, message: Message, client: RoleBot, finished: Function) => {
+  return new Promise((resolve, reject) => {
+    //@ts-ignore
+    const GUILD_ID = message.guild.id;
+    let id: string = "";
+    const { guild } = message;
+    if (!guild) throw new Error("Guild not found, possibly a group chat?");
+
+    /* Let me clarify I am disgusted with the code below */
+    channel
+      .send("Enter the role name. If you want to stop, say `cancel` or `done`.")
+      .then(bm => { 
+        // Because I'm fighting callbacks and I'm stupid
+        const emojiId = (i: string) => (id = i);
+
+        channel
+          .awaitMessages(m => m.author.id === message.author.id, {
+            max: 1,
+            time: 60000,
+            errors: ["time"]
+          })
+          .then(m => {
+            // Might as well cancel the whole process if they don't wanna do this
+            const msg = m.first();
+            if (!msg) throw new Error("Message somehow went missing");
+            const content = msg.content.toLowerCase();
+
+            if (
+              m &&
+              (content.includes("cancel") || content.includes("done")) &&
+              bm instanceof Message
+            ) {
+              finished();
+              bm.delete();
+              msg.delete();
+              return resolve("Cancelled");
+            }
+
+            const GUILD_REACT = guildReactions(guild.id);
+
+            const role = guild.roles.find(
+              r => r.name.toLowerCase() === content
+            );
+
+            if (!role && bm instanceof Message) {
+              bm.edit("Role not found, check if you typed it correctly.");
+              msg.delete();
+              setTimeout(() => {
+                bm.delete();
+              }, 5000);
+              return reject("Role not found");
+            }
+
+            if (
+              role &&
+              bm instanceof Message &&
+              GUILD_REACT.find(r => r.role_id === role.id)
+            ) {
+              bm.edit(`Emoji already exist for this role`);
+              setTimeout(() => {
+                bm.delete().catch(() => { });
+              }, 5000);
+              msg.delete().catch(() => { });
+              return reject("Emoji exist");
+            }
+
+            if (role && bm instanceof Message) {
+              // They got the role they wanted. Now we need to get the emoji
+              bm.edit(
+                "Now send the emoji to match the role. This must be local to the server or a generic Discord emoji."
+              );
+              msg.delete().catch(() => { });
+
+              channel
+                .awaitMessages(m => m.author.id === message.author.id, {
+                  max: 1,
+                  time: 20000,
+                  errors: ["time"]
+                })
+                .then(m => {
+                  const msg = m.first()
+                  if(!msg) throw new Error("Msg somehow gone")
+                  // Some discord emojis don't have id's and just use the unicode. Weird
+                  const match = /:(\d+)>/.exec(msg.content);
+
+
+                  if (
+                    m &&
+                    (content.includes("cancel") || content.includes("done")) &&
+                    bm instanceof Message
+                  ) {
+                    finished();
+                    bm.delete().catch(() => { });
+                    msg.delete().catch(() => { });
+                    return resolve("Cancelled");
+                  }
+
+                  if (match) {
+                    const [, id] = match;
+                    if (!client.emojis.get(id)) {
+                      bm.edit(
+                        `Either not an emoji or it's not available to me. :(`
+                      );
+                      setTimeout(() => {
+                        message.delete().catch(() => { });
+                        bm.delete().catch(() => { });
+                      }, 3000);
+                      msg.delete();
+                      return reject("Emoji not avail")
+                    }
+
+                    emojiId(id);
+                  } else {
+                    emojiId(msg.content);
+                  }
+
+                  if (role && id !== "") {
+                    // Assuming everything went uh, great. Try to add. :))
+                    addReactionRole(id, role.id, role.name, GUILD_ID);
+
+                    if (bm instanceof Message) {
+                      msg.delete().catch(() => { });
+                      setTimeout(() => {
+                        bm.delete().catch(() => { });
+                        message.delete().catch(() => { });
+                      }, 100);
+                      resolve("Added role.")
+                    }
+                  }
+                })
+                .catch(() => {
+                  if (bm instanceof Message) bm.edit("No usable emoji given.");
+                  reject(finished());
+                });
+            }
+          })
+          .catch(() => {
+            if (bm instanceof Message) bm.edit("You didn't send role name.");
+            reject(finished());
+          });
+      });
+  })
+}
---
+++ commands/reactCommands/remove.ts
@@ -0,0 +1,67 @@
+import { Message } from "discord.js";
+import { removeReactionRole, guildReactions } from "../../src/setup_table";
+import RoleBot from "../../src/bot";
+
+export default {
+  desc: "Delete a reaction role. Use `-all` to delete all current reactions.",
+  name: "-remove",
+  args: "<role name> or -all",
+  type: "reaction",
+  run: (message: Message, args: string[], client: RoleBot) => {
+    setTimeout(() => {
+      message.delete();
+    }, 5000);
+    if (!message.guild || (message.member && !message.member!.hasPermission(["MANAGE_ROLES"])))
+      return message.react("❌");
+    const arg = args.join(" ");
+    const GUILD_ID = message.guild.id;
+    const DB_ROLES = guildReactions(GUILD_ID).map(r => r.role_id);
+    const ROLE = message.guild.roles.find(
+      r => r.name.toLowerCase() === arg.toLowerCase()
+    );
+    const FOLDERS = client.guildFolders.get(GUILD_ID) || []
+    let found = false;
+
+    if (ROLE && DB_ROLES.includes(ROLE.id)) {
+      for(const f of FOLDERS) {
+        const folder = client.folderContents.get(f.id)
+
+        if(!folder) continue;
+
+        for(const r of folder.roles) {
+          if (r.role_id === ROLE.id) {
+            folder.roles.splice(folder.roles.indexOf(r), 1);
+            found = true;
+            client.folderContents.set(f.id, folder);
+            break;
+          }
+        }
+        if (found) break;
+      }
+      removeReactionRole(ROLE.id);
+      return message.react("✅");
+    } else if (arg.includes("-all")) {
+      DB_ROLES.forEach(r => {
+        for(const f of FOLDERS) {
+          found = false;
+          const folder = client.folderContents.get(f.id)
+
+          if(!folder) continue;
+
+          for(const role of folder.roles) {
+            if (role.role_id === r) {
+              folder.roles.splice(folder.roles.indexOf(r), 1);
+              found = true;
+              client.folderContents.set(f.id, folder);
+              break;
+            }
+          }
+        }
+        removeReactionRole(r)
+      });
+      return message.react("✅");
+    }
+
+    return message.react("❌");
+  }
+};
