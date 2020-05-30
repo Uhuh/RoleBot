af070a15b9e726eb1c09c7b18b353216a1d61c39
Dylan Warren <duwtgb@mst.edu>
2020-05-30 04:24:58 -0500

    Interfaces for db and just bot overall

---
+++ src/interfaces.ts
@@ -0,0 +1,41 @@
+export interface IReactionRole {
+  folder_id: string;
+  guild_id: string;
+  emoji_id: string;
+  role_id: string;
+  role_name: string;
+  id: string; 
+};
+
+export interface IRole {
+  id: string;
+  role_name: string;
+  guild: string;
+  role_id: string;
+};
+
+export interface IReactMessage {
+  message_id: string;
+  channel_id: string;
+  guild_id: string;
+};
+
+export interface IFolder {
+  id: number;
+  guild_id: string;
+  label: string;
+};
+
+export interface IRoleChannel {
+  id: string;
+  channel_id: string;
+  guild: string; // guild_id
+  message_id: string;
+};
+
+export interface IJoinRole {
+  id: string;
+  role_name: string;
+  role_id: string;
+  guild_id: string;
+};
