91cc3254fe5295436d36593c39e31cffaffc62e2
Dylan Warren <duwtgb@mst.edu>
2020-11-19 12:19:00 -0600

    fix join roles

--- commands/callable/join.ts
+++ commands/callable/join.ts
@@ -1,40 +1,41 @@
-import { Message } from "discord.js"
-import { joinRoles } from "../../src/setup_table"
-import RoleBot from "../../src/bot"
+import { Message } from 'discord.js';
+import { getJoinRoles, joinRoles } from '../../src/setup_table';
+import RoleBot from '../../src/bot';
 
 export default {
-  desc:
-    "Users are given this role upon joining.\n" +
-    "E.G: `@RoleBot join Member`",
-  name: "join",
-  args: "",
-  type: "message",
+  desc: 'Users are given this role upon joining.\n' + 'E.G: `rb join Member`',
+  name: 'join',
+  args: '',
+  type: 'message',
   run: (message: Message, args: string[], client: RoleBot) => {
     // ignore them plebians
-    if (
-      !message.guild ||
-      !message.member!.hasPermission(["MANAGE_ROLES"])
-    )
-      return
-    if (!args.length) return message.channel.send("No arguments provided.\n`@RoleBot role <type> <name>`")
-    const roleName = args.join(" ");
+    if (!message.guild || !message.member!.hasPermission(['MANAGE_ROLES']))
+      return;
+    if (!args.length)
+      return message.channel.send(
+        'No arguments provided.\n`@RoleBot role <type> <name>`'
+      );
+    const roleName = args.join(' ');
 
-    const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === roleName.toLowerCase())
+    const role = message.guild.roles.cache.find(
+      (r) => r.name.toLowerCase() === roleName.toLowerCase()
+    );
+    const guildJRoles = getJoinRoles(message.guild.id);
 
     if (role) {
-      message.react("✅");
-      client.joinRoles.get(message.guild.id)!.push({
-        role_id: role.id,
-        role_name: role.name
-      })
+      message.react('✅');
+      client.joinRoles.set(message.guild.id, [
+        ...guildJRoles,
+        { role_id: role.id, role_name: role.name },
+      ]);
       return joinRoles.run({
         id: `${message.guild.id}-${role.id}`,
         role_name: role.name,
         role_id: role.id,
-        guild_id: message.guild.id
+        guild_id: message.guild.id,
       });
     }
 
-    return message.react("❌");
-  }
-}
+    return message.react('❌');
+  },
+};
