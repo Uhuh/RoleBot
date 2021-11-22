40bb4ab9de127d3d6dcd49ad28915205b863271e
Dylan Warren <dylanwarren19@gmail.com>
2021-11-21 18:44:50 -0600

    Invite to the support server.

--- src/vars.ts
+++ src/vars.ts
@@ -4,3 +4,4 @@ export const DEV_MODE: string = process.env.DEV_MODE || '0';
 export const DEV_ID: string = '289151449412141076';
 export const DATABASE_TYPE: string = process.env.DATABASE_TYPE || 'RoleBotBeta';
 export const CLIENT_ID: string = '471811409886314496';
+export const SUPPORT_URL = 'https://discord.gg/2fEpJTWzAU';
