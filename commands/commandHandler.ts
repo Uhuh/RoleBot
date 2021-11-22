1cf8440a1bab966e6490ec3258e32a61c570886d
Dylan Warren <dylanwarren19@gmail.com>
2021-11-21 18:43:20 -0600

    Move to barrel imports, fs is so ugly and horrible lol

--- commands/commandHandler.ts
+++ commands/commandHandler.ts
@@ -1,78 +1,44 @@
 import RoleBot from '../src/bot';
-import * as fs from 'fs';
 import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import {
+  RESTPostAPIApplicationCommandsJSONBody,
+  Routes,
+} from 'discord-api-types/v9';
 import { TOKEN } from '../src/vars';
-import { Command } from '../utilities/types/commands';
 import { LogService } from '../src/services/logService';
+import * as categoryCommands from './category';
+import * as generalCommands from './general';
+import * as reactionCommands from './react';
+import * as slashGenerators from './slashGenerators';
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
 
 export default (client: RoleBot) => {
   LogService.setPrefix('SlashCommandHandler');
-
   LogService.logInfo(`Loading all slash commands...`);
 
-  const categoryCommands: string[] = [];
-  const generalCommands: string[] = [];
-  const reactionCommands: string[] = [];
-  const slashGenerators: string[] = [];
+  const commandsJson: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
-  const commandsJson: any = [];
-
-  // Read all the file names.
-  fs.readdirSync('commands/category/').forEach((file) =>
-    categoryCommands.push(file.slice(0, -3))
-  );
-
-  fs.readdirSync('commands/general/').forEach((file) =>
-    generalCommands.push(file.slice(0, -3))
-  );
-
-  fs.readdirSync('commands/react/').forEach((file) =>
-    reactionCommands.push(file.slice(0, -3))
-  );
-
-  fs.readdirSync('commands/slashGenerators/').forEach((file) =>
-    slashGenerators.push(file.slice(0, -3))
-  );
-
-  // Loop over each file, set the commands and generate their slash JSON.
-  for (const file of categoryCommands) {
-    const command: {
-      command: Command;
-    } = require(`./category/${file}`);
-    client.commands.set(command.command.name.toLowerCase(), command.command);
+  // Category commands don't have the data object since they depend on categorySlashGenerator
+  for (const cmd of Object.values(categoryCommands)) {
+    client.commands.set(cmd.name.toLowerCase(), cmd);
   }
 
-  for (const file of generalCommands) {
-    const command: {
-      command: Command;
-    } = require(`./general/${file}`);
-
-    client.commands.set(
-      command.command.data.name.toLowerCase(),
-      command.command
-    );
-    commandsJson.push(command.command.data.toJSON());
+  // Use the slash commands name generated from their data.
+  for (const cmd of [
+    ...Object.values(generalCommands),
+    ...Object.values(reactionCommands),
+  ]) {
+    client.commands.set(cmd.data.name.toLowerCase(), cmd);
   }
 
-  for (const file of reactionCommands) {
-    const command: {
-      command: Command;
-    } = require(`./react/${file}`);
-
-    client.commands.set(
-      command.command.data.name.toLowerCase(),
-      command.command
-    );
-
-    commandsJson.push(command.command.data.toJSON());
-  }
-
-  for (const file of slashGenerators) {
-    const command = require(`./slashGenerators/${file}`);
-    commandsJson.push(command.default.toJSON());
+  // Generate the JSON to send to the Discord SlashCommand API
+  for (const cmd of [
+    ...Object.values(generalCommands).map((c) => c.data),
+    ...Object.values(reactionCommands).map((c) => c.data),
+    ...Object.values(slashGenerators),
+  ]) {
+    commandsJson.push(cmd.toJSON());
   }
 
   // Make a request to Discord to create all the slash commands.
@@ -89,7 +55,9 @@ export default (client: RoleBot) => {
       );
       LogService.logOk(`Created slash commands successfully.`);
     } catch (e) {
-      LogService.logError(`Errored when trying to create slash commands. ${e}`);
+      LogService.logError(
+        `Errored when trying to create slash commands.\n${e}\n`
+      );
     }
   })();
 };
