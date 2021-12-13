7790ea402b87260782cd72561b9664ab7fd779ad
Dylan Warren <dylanwarren19@gmail.com>
2021-12-12 19:08:19 -0600

    Message command should just handle reacting to a single message.

--- commands/react/message.ts
+++ commands/react/message.ts
@@ -1,91 +1,88 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { ChannelType } from 'discord-api-types';
 import {
-  Interaction,
+  CommandInteraction,
   MessageActionRow,
   MessageSelectMenu,
+  Permissions,
   SelectMenuInteraction,
 } from 'discord.js';
 import { GET_GUILD_CATEGORIES } from '../../src/database/database';
-import { Category, DataCommand } from '../../utilities/types/commands';
+import { Category } from '../../utilities/types/commands';
+import { SlashCommand } from '../slashCommand';
 
-export const reactMessage: DataCommand = {
-  name: '/reaction-message',
-  desc: 'Send reaction roles to a specific channel. Have RoleBot react to a custom message instead of using the defaults.',
-  type: Category.react,
-  data: new SlashCommandBuilder()
-    .setName('reaction-message')
-    .setDescription('Set a custom message for your reaction roles.')
-    .addChannelOption((option) =>
-      option
-        .addChannelType(ChannelType.GuildText)
-        .setName('channel')
-        .setDescription('RoleBot will send its default messages')
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName('message-id')
-        .setDescription(
-          'Want rolebot to react to your message instead? Pass the message ID.'
-        )
-    ),
-  execute: async (interaction: Interaction) => {
+export class ReactMessageCommand extends SlashCommand {
+  constructor() {
+    super(
+      'reaction-message',
+      'Use this command to react with a specific category of roles to a message.',
+      Category.react,
+      [Permissions.FLAGS.MANAGE_ROLES]
+    );
+
+    this.addStringOption(
+      'message-link',
+      'Copy a message link and place it here for the message you want me to react to.',
+      true
+    );
+  }
+
+  execute = async (interaction: CommandInteraction) => {
     if (!interaction.isCommand()) return;
 
-    const channel = interaction.options.get('channel')?.channel;
-    // Optional message ID.
-    const messageId = interaction.options.get('message-id')?.value;
+    const messageLink = interaction.options.get('message-link')?.value;
 
-    // Let's make sure we can do anything related to the channel.
-    if (!channel) {
+    if (!messageLink || typeof messageLink !== 'string') {
       return await interaction.reply({
-        ephemeral: true,
-        content: `Hold on partner, it appears I couldn't find that channel. Try again with a channel I have access to.`,
-      });
-    } else if (channel.type !== 'GUILD_TEXT') {
-      return await interaction.reply({
-        ephemeral: true,
-        content: `Hey! I can only be utilized in **TEXT** channels! I'm no good in anything else.`,
+        content: `Hmm, I'm not what happened but I can't see the message link. Please try again.`,
       });
     }
 
-    if (messageId) {
-      /**
-       * @TODO - Verify the message ID before doing anything.
-       */
+    const [_, channelId, messageId] = messageLink.match(/\d+/g) ?? [];
 
-      const categories = await GET_GUILD_CATEGORIES(interaction.guildId);
+    const channel = await interaction.guild?.channels.fetch(channelId);
 
-      if (!categories.length) {
-        return await interaction.reply({
-          content: `It appears there are no categories! Try out \`/category create\` to create a category reaction pack to store and manage your roles much easier.`,
-        });
-      }
-
-      const selectMenu = new MessageActionRow().addComponents(
-        new MessageSelectMenu()
-          .setCustomId(`select-message`)
-          .setPlaceholder(`Pick a category to react with.`)
-          .addOptions(
-            categories.map((c, idx) => ({
-              label: c.name ?? `Category-${idx}`,
-              description: c.description ?? ``,
-              value: `message-${c.guildId}-${channel.id}-${messageId}-${c.categoryId}`,
-            }))
-          )
-      );
-
-      await interaction.reply({
-        content: `Let's make this easier for you. Select a category and I will use the reaction roles in that category to react to the message.`,
-        components: [selectMenu],
+    if (!channel || channel.type !== 'GUILD_TEXT') {
+      return await interaction.reply({
+        content: `Hey! I couldn't find that channel, make sure you're copying the message link right.`,
       });
     }
-  },
+
+    const message = await channel.messages.fetch(messageId);
+
+    if (!message) {
+      return await interaction.reply({
+        content: `Hey! I couldn't find that message, make sure you're copying the message link right.`,
+      });
+    }
+
+    const categories = await GET_GUILD_CATEGORIES(interaction.guildId);
+
+    if (!categories.length) {
+      return await interaction.reply({
+        content: `It appears there are no categories! Try out \`/category create\` to create a category reaction pack to store and manage your roles much easier.`,
+      });
+    }
+
+    const selectMenu = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId(`select-message`)
+        .setPlaceholder(`Pick a category to react with.`)
+        .addOptions(
+          categories.map((c, idx) => ({
+            label: c.name ?? `Category-${idx}`,
+            description: c.description ?? '',
+            value: `message-${c.guildId}-${channelId}-${messageId}-${c._id}`,
+          }))
+        )
+    );
+
+    await interaction.reply({
+      content: `Let's make this easier for you. Select a category and I will use the reaction roles in that category to react to the message.`,
+      components: [selectMenu],
+    });
+  };
+}
+
+export const handleMessageChoice = (interaction: SelectMenuInteraction) => {
+  const [_, guildId, channelId, messageId, categoryId] =
+    interaction.values[0].split('-');
 };
-
-export const handleMessageChoice = (
-  interaction: SelectMenuInteraction,
-  channelId: string,
-  messageId: string
-) => {};
