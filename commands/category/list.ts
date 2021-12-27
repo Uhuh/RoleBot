4e583896c8b761240c9ff570a7db830bc922b0eb
Dylan Warren <dylanwarren19@gmail.com>
2021-12-27 01:59:05 -0600

    Ability to list categories works

--- commands/category/list.ts
+++ commands/category/list.ts
@@ -1,10 +1,15 @@
-import { CommandInteraction, Permissions } from 'discord.js';
-import { Category } from '../../utilities/types/commands';
+import { CommandInteraction, MessageEmbed, Permissions } from 'discord.js';
+import { GET_GUILD_CATEGORIES } from '../../src/database/database';
 import { SlashCommand } from '../slashCommand';
+import { EmbedService } from '../../src/services/embedService';
+import { LogService } from '../../src/services/logService';
+import { Category } from '../../utilities/types/commands';
+import RoleBot from '../../src/bot';
 
 export class ListCategoryCommand extends SlashCommand {
-  constructor() {
+  constructor(client: RoleBot) {
     super(
+      client,
       'category-list',
       'List all your categories and the roles within them.',
       Category.category,
@@ -12,10 +17,39 @@ export class ListCategoryCommand extends SlashCommand {
     );
   }
 
-  execute = (interaction: CommandInteraction) => {
-    interaction.reply({
-      ephemeral: true,
-      content: 'Category-list responds.',
+  execute = async (interaction: CommandInteraction) => {
+    const categories = await GET_GUILD_CATEGORIES(interaction.guildId).catch(
+      (e) => {
+        LogService.error(
+          `Failed to get categories for guild[${interaction.guildId}]`
+        );
+        LogService.error(e);
+      }
+    );
+
+    if (!categories || !categories.length) {
+      LogService.debug(
+        `Guild[${interaction.guildId}] did not have any categories.`
+      );
+
+      return interaction.reply(
+        `Hey! It appears that there aren't any categories for this server... however, if there ARE supposed to be some and you see this please wait a second and try again.`
+      );
+    }
+
+    interaction.reply(
+      `Hey! Let me build these embeds for you real quick and send them...`
+    );
+
+    const embeds: MessageEmbed[] = [];
+
+    for (const cat of categories) {
+      embeds.push(EmbedService.categoryReactRoleEmbed(cat, this.client));
+    }
+
+    interaction.channel?.send({
+      content: `And here they are!`,
+      embeds,
     });
   };
 }
