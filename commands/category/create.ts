715151356cb00a16884d3d5954f1753e15050b75
Dylan Warren <dylanwarren19@gmail.com>
2021-12-27 01:25:32 -0600

    Ability to create categories work

--- commands/category/create.ts
+++ commands/category/create.ts
@@ -1,14 +1,17 @@
 import { CommandInteraction, Permissions } from 'discord.js';
+import RoleBot from '../../src/bot';
 import {
   CREATE_GUILD_CATEGORY,
   GET_CATEGORY_BY_NAME,
 } from '../../src/database/database';
+import { LogService } from '../../src/services/logService';
 import { Category } from '../../utilities/types/commands';
 import { SlashCommand } from '../slashCommand';
 
 export class CreateCategoryCommand extends SlashCommand {
-  constructor() {
+  constructor(client: RoleBot) {
     super(
+      client,
       'category-create',
       'Create a new category to categorize your reaction roles in.',
       Category.category,
@@ -26,25 +29,35 @@ export class CreateCategoryCommand extends SlashCommand {
       'category-desc'
     );
 
-    if (await GET_CATEGORY_BY_NAME(interaction.guildId, categoryName)) {
-      return await interaction.reply({
-        content: `Hey! It turns out you already have a category with that name made. Try checking it out.`,
-      });
-    }
-
     if (!categoryName) {
-      return await interaction.reply({
+      return interaction.reply({
         ephemeral: true,
-        content: `Hey! It appears I am struggling to find the name you sent me. Please try again.`,
+        content: `Hey! It says you submitted no category name! You need to submit that. Please try again.`,
       });
     }
 
-    CREATE_GUILD_CATEGORY(interaction.guildId, categoryName, categoryDesc).then(
-      () => {
-        interaction.reply({
-          content: `Hey! I successfully created the category for you!`,
-        });
-      }
-    );
+    if (await GET_CATEGORY_BY_NAME(interaction.guildId, categoryName)) {
+      return await interaction.reply(
+        `Hey! It turns out you already have a category with that name made. Try checking it out.`
+      );
+    }
+
+    CREATE_GUILD_CATEGORY(interaction.guildId, categoryName, categoryDesc)
+      .then(() => {
+        LogService.debug(
+          `Successfully created category[${categoryName}] for guild[${interaction.guildId}]`
+        );
+        interaction.reply(`Hey! I successfully created the category for you!`);
+      })
+      .catch((e) => {
+        LogService.error(
+          `Issue creating category[${categoryName}] for guild[${interaction.guildId}]`
+        );
+        LogService.error(e);
+
+        interaction.reply(
+          `Hey! I had some trouble creating that category for you. Please wait a minute and try again.`
+        );
+      });
   };
 }
