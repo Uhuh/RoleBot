c2ca49476719ededc6e1b16c272409cc39c90b72
Dylan Warren <dylanwarren19@gmail.com>
2021-12-27 01:59:41 -0600

    Basic information about the bot

--- commands/general/info.ts
+++ commands/general/info.ts
@@ -1,15 +1,37 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { AVATAR_URL, SUPPORT_URL, VOTE_URL } from '../../src/vars';
 import { Category } from '../../utilities/types/commands';
+import { COLOR } from '../../utilities/types/globals';
 import { SlashCommand } from '../slashCommand';
+import RoleBot from '../../src/bot';
 
 export class InfoCommand extends SlashCommand {
-  constructor() {
-    super('info', `RoleBot's invite, ping, etc.`, Category.general);
+  constructor(client: RoleBot) {
+    super(client, 'info', `RoleBot's invite, ping, etc.`, Category.general);
   }
   execute = (interaction: CommandInteraction) => {
+    const embed = new MessageEmbed();
+
+    embed
+      .setTitle('General info for Vivi')
+      .setColor(COLOR.AQUA)
+      .setDescription(
+        `
+Thanks for using RoleBot!
+Server count: ${this.client.guilds.cache.size} servers.
+Latency is ${
+          Date.now() - interaction.createdTimestamp
+        }ms. API Latency is ${Math.round(this.client.ws.ping)}ms.
+
+[Click to Vote!](${VOTE_URL})
+[Join the support server!](${SUPPORT_URL})
+`
+      )
+      .setThumbnail(AVATAR_URL);
+
     interaction.reply({
-      ephemeral: true,
-      content: 'Info responds.',
+      content: `Here's some info about me.`,
+      embeds: [embed],
     });
   };
 }
