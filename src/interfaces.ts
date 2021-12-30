52c7e56fbcf1c1676df851becb48c7a9d30b987e
Dylan Warren <dylanwarren19@gmail.com>
2021-12-30 13:46:25 -0600

    Remove all the old interfaces from the original design

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,3 @@
-export interface IReactionRole {
-  folder_id: string;
-  guild_id: string;
-  emoji_id: string;
-  role_id: string;
-  role_name: string;
-  id: string;
-}
-
 export enum Colors {
   aqua = 0x00ffff,
   black = 0x000000,
@@ -15,47 +6,4 @@ export enum Colors {
   red = 0xff0000,
   green = 0x00ff00,
   blue = 0x0000ff,
-}
-
-export interface IRole {
-  id: string;
-  role_name: string;
-  guild: string;
-  role_id: string;
-}
-
-export interface IReactMessage {
-  message_id: string;
-  channel_id: string;
-  guild_id: string;
-}
-
-export interface IFolder {
-  id: number;
-  guild_id: string;
-  label: string;
-}
-
-export interface IFolderReactEmoji extends IFolder {
-  roles: IRoleEmoji[];
-}
-
-export interface IRoleEmoji {
-  role_id: string;
-  role_name: string;
-  emoji_id: string;
-}
-
-export interface IRoleChannel {
-  id: string;
-  channel_id: string;
-  guild: string; // guild_id
-  message_id: string;
-}
-
-export interface IJoinRole {
-  id: string;
-  role_name: string;
-  role_id: string;
-  guild_id: string;
 }
