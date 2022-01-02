7ee65836b7180140dc0729756c54599cdeedb261
Dylan Warren <dylanwarren19@gmail.com>
2022-01-01 18:16:14 -0600

    Use clients logging server, no need to create a new one here

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import RB from './bot';
-import { LogService } from './services/logService';
 
 const RoleBot = new RB();
-const log = new LogService(`BotStart:Index`);
 
 RoleBot.start().catch((e) => {
-  log.error(`RoleBot has encounter an error while starting up. ${e}`);
+  RoleBot.log.error(`RoleBot has encounter an error while starting up. ${e}`);
 });
 
 export default RoleBot;
