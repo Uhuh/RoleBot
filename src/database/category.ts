55583fee0927679fbebaa4e202edde86da28b5e7
Dylan Warren <dylanwarren19@gmail.com>
2022-01-01 18:16:43 -0600

    Going to do some category mutuallyExclusive stuff

--- src/database/category.ts
+++ src/database/category.ts
@@ -4,12 +4,14 @@ const Category = new Schema({
   guildId: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, maxlength: 1024, default: '' },
+  mutuallyExclusive: { type: Boolean, default: false },
 });
 
 export interface ICategory {
   guildId: string;
   name: string;
   description: string;
+  mutuallyExclusive: boolean;
 }
 
 export interface ICategoryDoc extends ICategory, Document {}
