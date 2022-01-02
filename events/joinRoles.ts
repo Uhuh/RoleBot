0a9e604543a2d20a35e9381c00af06d846236f17
Dylan Warren <dylanwarren19@gmail.com>
2022-01-01 18:14:41 -0600

    I need to do join roles sometime

--- events/joinRoles.ts
+++ events/joinRoles.ts
@@ -1,14 +1,6 @@
-import { GuildMember, Collection } from 'discord.js';
+import { GuildMember } from 'discord.js';
+import RoleBot from '../src/bot';
 
-export default async (
-  member: GuildMember,
-  joinRoles: Collection<string, string[]>
-) => {
-  for (const role of joinRoles.get(member.guild.id) || []) {
-    await member.guild.roles.fetch(role);
-    const joinRole = member.guild.roles.cache.get(role || '');
-    if (joinRole) {
-      member.roles.add(joinRole).catch(() => {});
-    }
-  }
+export default async (member: GuildMember, client: RoleBot) => {
+  // Handle this whenever
 };
