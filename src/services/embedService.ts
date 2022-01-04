97379755eb52f181cbcb6288998f152cb7d68658
Dylan Warren <dylanwarren19@gmail.com>
2022-01-03 19:57:14 -0600

    Make it clear what the description is when listing

--- src/services/embedService.ts
+++ src/services/embedService.ts
@@ -59,7 +59,7 @@ export class EmbedService {
     embed
       .setTitle(category.name)
       .setDescription(
-        `Mutually exclusive: **${category.mutuallyExclusive}**\n\n${category.description}\n\n${reactRoles}`
+        `Mutually exclusive: **${category.mutuallyExclusive}**\n\nDesc: **${category.description}**\n\n${reactRoles}`
       )
       .setColor(COLOR.DEFAULT);
 
