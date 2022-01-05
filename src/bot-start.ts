b86c50decd05821a4fcd4dbfd08225b806c315ec
Dylan Warren <dylanwarren19@gmail.com>
2022-01-05 08:38:51 -0600

    Change filename for sharding

---
+++ src/bot-start.ts
@@ -0,0 +1,9 @@
+import RB from './bot';
+
+const RoleBot = new RB();
+
+RoleBot.start().catch((e) => {
+  RoleBot.log.error(`RoleBot has encounter an error while starting up. ${e}`);
+});
+
+export default RoleBot;
