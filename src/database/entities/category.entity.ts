b5aaadd56cdc0ec5bb7a9d1b5e2964e5281134b7
Dylan Warren <dylanwarren19@gmail.com>
2022-01-06 00:41:05 -0600

    Unused code

--- src/database/entities/category.entity.ts
+++ src/database/entities/category.entity.ts
@@ -1,10 +1,4 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-  Unique,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 export interface ICategory {
   guildId: string;
