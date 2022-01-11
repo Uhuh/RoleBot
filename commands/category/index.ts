4293fe75069c87f0f0400431386c3970687b250c
Dylan Warren <dylanwarren19@gmail.com>
2022-01-10 23:39:30 -0600

    Ability to update an category embed

--- commands/category/index.ts
+++ commands/category/index.ts
@@ -3,3 +3,4 @@ export { CreateCategoryCommand } from './create';
 export { ListCategoryCommand } from './list';
 export { RemoveCategoryCommand } from './remove';
 export { EditCategoryCommand } from './edit';
+export { UpdateCategoryCommand } from './update';
