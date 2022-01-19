0b669a7152b90aa3d899a4e893b892f1bc79811a
Dylan Warren <dylanwarren19@gmail.com>
2022-01-19 16:46:22 -0600

    Stupid packets.

--- events/raw_packet.ts
+++ events/raw_packet.ts
@@ -1,46 +1,53 @@
 import { TextChannel } from 'discord.js';
 import RoleBot from '../src/bot';
+import { LogService } from '../src/services/logService';
+
+const log = new LogService('RawPacket');
 
 export const handle_packet = async (packet: any, client: RoleBot) => {
-  if (
-    !packet.t ||
-    (packet.t !== 'MESSAGE_REACTION_ADD' &&
-      packet.t !== 'MESSAGE_REACTION_REMOVE')
-  ) {
-    return;
-  }
-  const channel = await client.channels.fetch(packet.d.channel_id, {
-    cache: false,
-  });
-  if (!channel || channel.type !== 'GUILD_TEXT') {
-    return;
-  }
-  //Ignore if messages are cached already
-  if ((channel as TextChannel).messages.cache.has(packet.d.message_id)) {
-    return;
-  }
-
-  const message = await (channel as TextChannel).messages.fetch(
-    packet.d.message_id,
-    {
-      cache: false,
+  try {
+    if (
+      !packet.t ||
+      (packet.t !== 'MESSAGE_REACTION_ADD' &&
+        packet.t !== 'MESSAGE_REACTION_REMOVE')
+    ) {
+      return;
     }
-  );
+    const channel = await client.channels.fetch(packet.d.channel_id, {
+      cache: false,
+    });
+    if (!channel || channel.type !== 'GUILD_TEXT') {
+      return;
+    }
+    //Ignore if messages are cached already
+    if ((channel as TextChannel).messages.cache.has(packet.d.message_id)) {
+      return;
+    }
 
-  const react = message.reactions.cache.get(
-    //Emojis without a unicode name must be referenced
-    packet.d.emoji.id || packet.d.emoji.name
-  );
-  const user = await client.users.fetch(packet.d.user_id);
+    const message = await (channel as TextChannel).messages.fetch(
+      packet.d.message_id,
+      {
+        cache: false,
+      }
+    );
 
-  // Just... ignore react if it doesn't exist anymore????
-  if (user.bot || !react) {
-    return; // Ignore bots. >:((((
-  }
+    const react = message.reactions.cache.get(
+      //Emojis without a unicode name must be referenced
+      packet.d.emoji.id || packet.d.emoji.name
+    );
+    const user = await client.users.fetch(packet.d.user_id);
 
-  if (packet.t === 'MESSAGE_REACTION_ADD') {
-    client.emit('messageReactionAdd', react, user);
-  } else if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-    client.emit('messageReactionRemove', react, user);
+    // Just... ignore react if it doesn't exist anymore????
+    if (user.bot || !react) {
+      return; // Ignore bots. >:((((
+    }
+
+    if (packet.t === 'MESSAGE_REACTION_ADD') {
+      client.emit('messageReactionAdd', react, user);
+    } else if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+      client.emit('messageReactionRemove', react, user);
+    }
+  } catch {
+    log.error(`Issue handling packet.`);
   }
 };
