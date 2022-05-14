a5c982ff2bcfdfaee2b1ff86c8f99716a6cb6be4
Dylan Warren <dylanwarren19@gmail.com>
2022-05-14 17:10:10 -0500

    Add owner Id and new avatar url

--- src/vars.ts
+++ src/vars.ts
@@ -1,8 +1,9 @@
 // Wonderfully hardcoded things.
+export const OWNER_ID = '289151449412141076';
 export const CLIENT_ID = process.env.CLIENT_ID || '493668628361904139';
 export const SUPPORT_URL = 'https://discord.gg/9BYN266sC4';
 export const AVATAR_URL =
-  'https://cdn.discordapp.com/avatars/471811409886314496/e46c68a64b88316435adf4e1ac0402d0.webp?size=2048';
+  'https://cdn.discordapp.com/avatars/493668628361904139/712f1bc1af7f54da4693f0c361444244.webp?size=2048';
 export const VOTE_URL = `https://top.gg/bot/${CLIENT_ID}/vote`;
 export const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=493668628361904139&scope=bot%20applications.commands&permissions=2416035904`;
 
