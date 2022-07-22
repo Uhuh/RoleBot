ce0bd6a7f3a21a937c299e2c826bfe402c977705
Dylan Warren <dylanwarren19@gmail.com>
2022-07-22 03:50:57 -0500

    chore: Unused file

--- events/joinRoles.ts
+++
@@ -1,6 +0,0 @@
-import { GuildMember } from 'discord.js-light';
-import RoleBot from '../src/bot';
-
-export default async (_member: GuildMember, _client: RoleBot) => {
-  // Handle this whenever
-};
