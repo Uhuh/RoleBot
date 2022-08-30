c1fcbf6e12d34cc581f6137f4ac78f162daf7852
Dylan Warren <dylanwarren19@gmail.com>
2022-08-30 00:01:58 -0500

    update: api update

--- commands/commandHandler.ts
+++ commands/commandHandler.ts
@@ -1,18 +1,15 @@
 import RoleBot from '../src/bot';
 import { REST } from '@discordjs/rest';
-import {
-  RESTPostAPIApplicationCommandsJSONBody,
-  Routes,
-} from 'discord-api-types/v9';
+import { RESTPostAPIApplicationCommandsJSONBody, Routes } from 'discord.js';
 import { CLIENT_ID, TOKEN } from '../src/vars';
 import { LogService } from '../src/services/logService';
 import * as categoryCommands from './category';
 import * as generalCommands from './general';
 import * as reactionCommands from './react';
 
-const rest = new REST({ version: '9' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
-export default (client: RoleBot) => {
+export default async (client: RoleBot) => {
   const log = new LogService('SlashCommandHandler');
   log.info(`Loading all slash commands...`);
 
@@ -29,23 +26,23 @@ export default (client: RoleBot) => {
   }
 
   // Deleting global commands. (:
-  //deleteSlashCommands();
+  await deleteSlashCommands();
 
   // Generate global slash commands
-  //generateSlashCommands(commandsJson);
+  await generateSlashCommands(commandsJson);
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function generateSlashCommands(
   commandsJson: RESTPostAPIApplicationCommandsJSONBody[]
 ) {
   const log = new LogService('GenerateSlashCommands');
   // Make a request to Discord to create all the slash commands.
   try {
-    await rest.put(Routes.applicationCommands(CLIENT_ID), {
+    const data = (await rest.put(Routes.applicationCommands(CLIENT_ID), {
       body: commandsJson,
-    });
-    log.info(`Created slash commands successfully.`);
+    })) as [];
+
+    log.info(`Successfully reloaded ${data?.length} application (/) commands.`);
   } catch (e) {
     log.error(`Errored when trying to create slash commands.\n${e}\n`);
   }
