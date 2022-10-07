1752787818ca302adfc3da0d9c5e750a40e81e85
Dylan Warren <dylanwarren19@gmail.com>
2022-10-07 01:21:00 -0500

    update: little bit better on commmand updating

--- commands/commandHandler.ts
+++ commands/commandHandler.ts
@@ -34,7 +34,9 @@ export const buildSlashCommands = async (
       ? Routes.applicationGuildCommands(CLIENT_ID, SERVER_ID)
       : Routes.applicationCommands(CLIENT_ID);
 
-    //await deleteSlashCommands(route);
+    log.info(`Using route '${route}'`);
+
+    await deleteSlashCommands(route);
     await generateSlashCommands(route, commandsJson);
   }
 };
