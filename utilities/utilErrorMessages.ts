f4eac5d0fc21abe4186403f12b397dd716bb5874
Dylan Warren <dylanwarren19@gmail.com>
2022-10-07 23:42:28 -0500

    feat: easier permission error generating

---
+++ utilities/utilErrorMessages.ts
@@ -0,0 +1,15 @@
+import errors from './json/errors.json';
+import { codeBlock } from 'discord.js';
+import { ChannelPing } from './utilPings';
+
+export const requiredPermissions = (channelId: string) => {
+  const resultString = errors['missing_permissions']
+    .replace(/{{channel}}/g, ChannelPing(channelId))
+    .replace(/{{permissions}}/g, codeBlock(errors['required_permissions']))
+    .replace(
+      /{{permission_reasons}}/g,
+      codeBlock(errors['permission_reasons'])
+    );
+
+  return resultString;
+};
