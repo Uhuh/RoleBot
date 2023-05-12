fe03a568481958c3021db64240364ceb1c7fd787
Dylan Warren <dylanwarren19@gmail.com>
2023-05-12 18:22:32 -0500

    fix: webhooks and environment have not been playing nice (#42)

--- utilities/types/globals.ts
+++ utilities/types/globals.ts
@@ -1,10 +1,3 @@
-import { WebhookClient } from 'discord.js';
-import {
-  GUILD_WEBHOOK_ID,
-  GUILD_WEBHOOK_TOKEN,
-  WEBHOOK_ID,
-  WEBHOOK_TOKEN,
-} from '../../src/vars';
 // Discord embed sidebar colors.
 export enum COLOR {
   DEFAULT = 15158332,
@@ -13,14 +6,3 @@ export enum COLOR {
   GREEN = 3066993,
   AQUA = 2025424,
 }
-
-// Because of sharding we can't reliably get the guild channel. Also this is actually so much easier!
-export const RoleBotErrorEventsWebhook = new WebhookClient({
-  id: WEBHOOK_ID,
-  token: WEBHOOK_TOKEN,
-});
-
-export const RoleBotGuildEventsWebhook = new WebhookClient({
-  id: GUILD_WEBHOOK_ID,
-  token: GUILD_WEBHOOK_TOKEN,
-});
