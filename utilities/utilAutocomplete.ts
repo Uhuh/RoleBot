912a0448817194d9abc0646ccf6fe17d7ce73576
Dylan Warren <dylanwarren19@gmail.com>
2023-09-10 15:42:01 -0500

    update: auto complete for react roles

--- utilities/utilAutocomplete.ts
+++ utilities/utilAutocomplete.ts
@@ -1,5 +1,6 @@
 import { AutocompleteInteraction } from 'discord.js';
 import { GET_GUILD_CATEGORIES } from '../src/database/queries/category.query';
+import { GET_REACT_ROLES_BY_GUILD } from '../src/database/queries/reactRole.query';
 
 export async function handleAutocompleteCategory(
   interaction: AutocompleteInteraction
@@ -8,10 +9,25 @@ export async function handleAutocompleteCategory(
 
   const categories = await GET_GUILD_CATEGORIES(interaction.guildId);
 
-  const focusedValue = interaction.options.getFocused();
-  const filtered = categories.filter((c) => c.name.startsWith(focusedValue));
+  const focusedValue = interaction.options.getFocused().toLowerCase();
+  const filtered = categories.filter((c) => c.name.toLowerCase().startsWith(focusedValue));
 
   return interaction.respond(
     filtered.map((c) => ({ name: c.name, value: `${c.id}` }))
+  );
+}
+
+export async function handleAutocompleteReactRoles(
+  interaction: AutocompleteInteraction
+) {
+  if (!interaction.guildId) return;
+
+  const reactRoles = await GET_REACT_ROLES_BY_GUILD(interaction.guildId);
+
+  const focusedValue = interaction.options.getFocused().toLowerCase();
+  const filtered = reactRoles.filter((rr) => rr.name.toLowerCase().startsWith(focusedValue));
+
+  return interaction.respond(
+    filtered.map((rr) => ({ name: rr.name, value: `${rr.id}` }))
   );
 }
