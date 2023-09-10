299c5d5dd8c46ee0905dc1b93df46389ba6c3ffd
Dylan Warren <dylanwarren19@gmail.com>
2023-09-10 15:42:13 -0500

    feat: entity for LinkedRole

---
+++ src/database/entities/link.entity.ts
@@ -0,0 +1,18 @@
+﻿import { BaseEntity, Column, Entity, Index, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ReactRole } from './reactRole.entity';
+
+@Entity()
+export class LinkedRole extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Index()
+  @Column()
+  guildId: string;
+
+  @Column()
+  roleId: string;
+
+  @ManyToMany(() => ReactRole, (reactRole) => reactRole.linkedRoles)
+  reactRole: ReactRole;
+}
